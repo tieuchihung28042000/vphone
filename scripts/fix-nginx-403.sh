9045f1ba893a16836e99e9879cd35c1fe16382cc
Chí Hùng <tieuchihung28042000@gmail.com>
2025-07-01 15:05:40 +0700

    Update: 2025-07-01 15:05:40

--- scripts/fix-nginx-403.sh
+++ scripts/fix-nginx-403.sh
@@ -92,7 +92,7 @@ server {
 
     # API proxy to backend
     location /api/ {
-        proxy_pass http://localhost:3000/api/;
+        proxy_pass http://localhost:4000/api/;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection 'upgrade';
